125baba6791fe14e0b72377b443ac58151284f1d
Oblackmon <oblackmo@andrew.cmu.edu>
2020-08-06 19:55:38 -0400

    Setting up webserver

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const express = require('express');
 
 const app = express();
 const host = 'localhost';
-const port = process.env.PORT;
-
-if (port == null || port == ""){
-  port = 8000;
-}
 
 app.listen(port);
 
@@ -15,6 +10,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server listening at ${port}");
 })
