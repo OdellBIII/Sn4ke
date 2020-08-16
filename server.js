8701e632fb295899bcc418db53daac02f32c12bc
Oblackmon <oblackmo@andrew.cmu.edu>
2020-08-16 10:03:03 -0400

    Added timer, random food generation, and control pad

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const startPosition1 = {x : 100, y : 400};
 const startPosition2 = {x : 700, y : 400};
 
 io.on('connection', (socket) => {
-
+  console.log("Connection made...");
   socket.on('message-from-client-to-serve', (msg) => {
     console.log(msg);
   });
@@ -35,13 +35,31 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('other-player-movement', data);
   });
+
+  socket.on('consume-food', (data) => {
+    socket.broadcast.emit('consume-food');
+
+    setTimeout(function(data){
+
+      var x = Math.ceil(Math.random() * 1000);
+      var y = Math.ceil(Math.random() * 600);
+      io.emit('make-food', {x : x, y : y});
+    }, 3000);
+  });
+
   if (counter == 0)
   {
     socket.emit('start-position', startPosition1);
+    //socket.emit('enable-player-2', startPosition2);
     counter = 1;
   }else{
     socket.emit('start-position', startPosition2);
-    socket.broadcast.emit('enable-player-2', startPosition2);
+    //socket.emit('enable-player-2', startPosition1);
+
+    setTimeout(function(data) {
+      io.emit('start-match');
+      io.emit('make-food', {x : 100, y : 100});
+    } , 3000);
     counter = 0;
   }
 });
