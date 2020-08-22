91c1a916dae6eb9f97d2e63c3dcd79b3faf38926
Oblackmon <oblackmo@andrew.cmu.edu>
2020-08-22 11:52:18 -0400

    Added four players of different colors with different spawn points

--- server.js
+++ server.js
@@ -21,13 +21,14 @@ app.get("/", (req, res) => {
 });
 
 
-const colors = [0xFF0000, 0x0000FF, 0x00FF00];
+const colors = [0xFF0000, 0x0000FF, 0x00FF00, 0xA87343];
 
-const startPosition1 = {x : 100, y : 400, color : colors[0], ticker : 15, speedBoost : 10, name : "player-1", playerNumber : "player-1"};
-const startPosition2 = {x : 700, y : 400, color : colors[1], ticker : 15, speedBoost : 10, name : "player-2", playerNumber : "player-2"};
-const startPosition3 = {x : 400, y : 200, color : colors[2], ticker : 15, speedBoost : 10, name : "player-3", playerNumber : "player-3"};
+const startPosition1 = {x : 200, y : 400, color : colors[0], ticker : 15, speedBoost : 10, name : "player-1", playerNumber : "player-1"};
+const startPosition2 = {x : 400, y : 400, color : colors[1], ticker : 15, speedBoost : 10, name : "player-2", playerNumber : "player-2"};
+const startPosition3 = {x : 600, y : 400, color : colors[2], ticker : 15, speedBoost : 10, name : "player-3", playerNumber : "player-3"};
+const startPosition4 = {x : 800, y : 400, color : colors[3], ticker : 15, speedBoost : 10, name : "player-4", playerNumber : "player-4"};
 
-const other_snakes = {player1 : startPosition1, player2 : startPosition2, player3 : startPosition3};
+const other_snakes = {player1 : startPosition1, player2 : startPosition2, player3 : startPosition3, player4 : startPosition4};
 
 
 // Generate food with random x,y coordinates and random color
@@ -43,7 +44,7 @@ function generateRandomFood(width, height, roomno, colors){
 var roomno = 1;
 io.on('connection', (socket) => {
 
-  if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms['room-'+roomno].length > 2)
+  if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms['room-'+roomno].length > 3)
   {
     roomno += 1;
   }
@@ -55,6 +56,7 @@ io.on('connection', (socket) => {
   startPosition1.roomno = roomno;
   startPosition2.roomno = roomno;
   startPosition3.roomno = roomno;
+  startPosition4.roomno = roomno;
 
   // Handling user input from client and emitting to other players
   socket.on('other-player-movement', (data) => {
@@ -96,6 +98,12 @@ io.on('connection', (socket) => {
   else if(counter == 2)
   {
     socket.emit('start-position', startPosition3);
+    counter += 1;
+  }
+  else if(counter == 3)
+  {
+
+    socket.emit('start-position', startPosition4);
 
     io.in("room-"+roomno).emit('make-food', {x : 500, y : 300});
     io.in("room-"+roomno).emit("enable-other-players", other_snakes);
