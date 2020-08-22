8614840fd0245a5ffd9f7f9cc1e898130b722464
Oblackmon <oblackmo@andrew.cmu.edu>
2020-08-22 11:22:25 -0400

    Added 3 player rooms. Working on 4 player rooms next

--- server.js
+++ server.js
@@ -21,10 +21,14 @@ app.get("/", (req, res) => {
 });
 
 
+const colors = [0xFF0000, 0x0000FF, 0x00FF00];
 
-const startPosition1 = {x : 100, y : 400, color : 0xFF0000, ticker : 15, speedBoost : 10};
-const startPosition2 = {x : 700, y : 400, color : 0x0000FF, ticker : 15, speedBoost : 10};
-const colors = [0xFF0000, 0x0000FF];
+const startPosition1 = {x : 100, y : 400, color : colors[0], ticker : 15, speedBoost : 10, name : "player-1", playerNumber : "player-1"};
+const startPosition2 = {x : 700, y : 400, color : colors[1], ticker : 15, speedBoost : 10, name : "player-2", playerNumber : "player-2"};
+const startPosition3 = {x : 400, y : 200, color : colors[2], ticker : 15, speedBoost : 10, name : "player-3", playerNumber : "player-3"};
+
+const other_snakes = {player1 : startPosition1, player2 : startPosition2, player3 : startPosition3};
+
 
 // Generate food with random x,y coordinates and random color
 function generateRandomFood(width, height, roomno, colors){
@@ -39,7 +43,7 @@ function generateRandomFood(width, height, roomno, colors){
 var roomno = 1;
 io.on('connection', (socket) => {
 
-  if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms['room-'+roomno].length > 1)
+  if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms['room-'+roomno].length > 2)
   {
     roomno += 1;
   }
@@ -50,6 +54,8 @@ io.on('connection', (socket) => {
 
   startPosition1.roomno = roomno;
   startPosition2.roomno = roomno;
+  startPosition3.roomno = roomno;
+
   // Handling user input from client and emitting to other players
   socket.on('other-player-movement', (data) => {
 
@@ -69,10 +75,8 @@ io.on('connection', (socket) => {
   // Handling when the connected client collides with another client
   socket.on('snake-collision', function(data){
 
-    startPosition1.name = "player-2";
     socket.to("room-"+data.roomno).emit('snake-collsion', startPosition1);
 
-    startPosition1.name = "player-1";
     socket.emit('snake-collision', startPosition1);
   });
 
@@ -82,16 +86,21 @@ io.on('connection', (socket) => {
   {
     socket.emit('start-position', startPosition1);
     //socket.emit('enable-player-2', startPosition2);
-    counter = 1;
+    counter += 1;
   }
-  else
+  else if(counter == 1)
   {
     socket.emit('start-position', startPosition2);
-    socket.emit('enable-player-2', startPosition1);
-    socket.to("room-"+roomno).emit('enable-player-2', startPosition2);
+    counter += 1;
+  }
+  else if(counter == 2)
+  {
+    socket.emit('start-position', startPosition3);
 
     io.in("room-"+roomno).emit('make-food', {x : 500, y : 300});
+    io.in("room-"+roomno).emit("enable-other-players", other_snakes);
     io.in("room-"+roomno).emit('start-match');
     counter = 0;
   }
+
 });
